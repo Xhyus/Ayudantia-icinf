e1761f57c0314524f655bc38d93f455317e96ef2
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-10-19 19:57:57 -0300

    Creacion de modelos

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: [
+            'admin',
+            'user'
+        ],
+        default: 'user'
+    },
+    cart: {
+        type: Schema.Types.ObjectId,
+        ref: 'cart',
+        default: []
+    }
+})
+
+module.exports = mongoose.model('user', userSchema);
