735896f8f993638205b5f61e5b6391e68aa3a2eb
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-11-25 21:45:51 -0300

    Small fix

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -2,10 +2,21 @@ const Category = require('../models/category');
 const Product = require('../models/product');
 
 const createCategory = (req, res) => {
-    const { name } = req.body // ["Hola","Mundo"]
     const { id } = req.params
-    name.map((nameCategory) => {
-
+    const { name } = req.body
+    const newCategory = new Category({
+        name
+    })
+    newCategory.save((err, category) => {
+        if (err) {
+            return res.status(400).send({ message: "Error al crear la categoria" })
+        }
+        Product.findByIdAndUpdate(id, { $push: { categories: category._id } }, { new: true }, (err, product) => {
+            if (err) {
+                return res.status(400).send({ message: "Error al actualizar el producto" })
+            }
+            return res.status(201).send(product)
+        })
     })
 }
 
